3736a07ee56cf7f5eee8b3c2f6121a57da1971e6
pedro.neres <showmediapublicidade@gmail.com>
2023-04-19 12:13:27 -0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
     }
 
     /**
@@ -28,9 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-
-        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
-        }
+        
     }
 }
